50e620a1a5735b8bf3dcbe64a779ae84a5d0ddc2
Jinman Lee <iam@mintplo.me>
2019-12-08 19:41:03 +0900

    리뷰 데이터 획득

---
+++ request.py
@@ -0,0 +1,50 @@
+# Standard library imports
+import csv
+import datetime as dt
+import json
+import os
+import statistics
+import time
+
+# Third Party library imports
+import requests
+from requests.exceptions import SSLError
+
+
+def get_request(url, parameters=None):
+    """Return json-formatted response of a get request using optional parameters.
+
+    Parameters
+    ----------
+    url : string
+    parameters : {'parameter': 'value'}
+        parameters to pass as part of get request
+
+    Returns
+    -------
+    json_data
+        json-formatted response (dict-like)
+    """
+    parameters['json'] = 1
+
+    try:
+        response = requests.get(url=url, params=parameters)
+    except SSLError as s:
+        print('SSL Error:', s)
+
+        for i in range(5, 0, -1):
+            print('\rWaiting... ({})'.format(i), end='')
+            time.sleep(1)
+        print('\rRetrying.' + ' ' * 10)
+
+        # recusively try again
+        return get_request(url, parameters)
+
+    if response:
+        return response.json()
+    else:
+        # response is none usually means too many requests. Wait and try again
+        print('No response, waiting 10 seconds...')
+        time.sleep(10)
+        print('Retrying.')
+        return get_request(url, parameters)
---
+++ scrap_steam_reviews.py
@@ -0,0 +1,51 @@
+# Standard library imports
+import os
+import time
+
+# Third Party library imports
+import pandas as pd
+
+# Customized imports
+import request
+
+url = "https://store.steampowered.com/appreviews/578080"
+parameters = {
+    'num_per_page': 100,
+    'review_type': 'negative',
+    'day_range': '730',
+    'filter': 'all',
+    'cursor': '*'
+}
+
+positive_reviews = pd.DataFrame()
+"""
+Scrap Response Review
+"""
+for _ in range(0, 51):
+    print(parameters)
+    response = request.get_request(url=url, parameters=parameters)
+    reviews_df = pd.DataFrame(response['reviews'])
+    positive_reviews = pd.concat([positive_reviews, reviews_df], axis=0)
+    positive_reviews = positive_reviews.drop_duplicates(['recommendationid'],
+                                                        keep='first')
+    print(positive_reviews)
+    # print(response['cursor'])
+
+    # IP 블락킹을 피하기 위해 sleeping
+    time.sleep(10)
+    """
+    다음 리뷰 스크래핑을 위한 CURSOR 할당
+    - 다음 리뷰가 없는 경우 스크랩 중단
+    """
+    next_page_cursor = response['cursor']
+    if not next_page_cursor:
+        break
+
+    parameters['cursor'] = next_page_cursor
+    print(parameters['cursor'])
+"""
+Positive Reviews CSV Filename for export
+"""
+dirname = os.path.dirname(__file__)
+csv_filename = os.path.join(dirname, './negative_reviews.csv')
+positive_reviews.to_csv(csv_filename, mode='w')
